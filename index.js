f2c5bf6ff6df8661eb41f6f7f70f289ac7f3414f
Peter Coughlin <coughlin1966@gmail.com>
2017-11-15 03:06:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var name = "Susan"
 var height = 70
-if (name === "Susan") {
-  console.log ("The name is correct")
-} //else {
+//if (name === "Susan") {
+//console.log ("The name is correct")
+//}
+//else {
 //  console.log ("Expected the name to be Susan")
 //}
 
